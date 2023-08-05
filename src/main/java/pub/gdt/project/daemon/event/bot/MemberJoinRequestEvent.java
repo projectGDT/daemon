4202d9fb038b1d09d110f4d280c3e16ee4cfb946
Wesley F. Young <willamwei@outlook.com>
2023-08-05 08:55:41 +0000

    create MemberJoinRequestEvent

--- src/main/java/pub/gdt/project/daemon/event/bot/MemberJoinRequestEvent.java
+++ src/main/java/pub/gdt/project/daemon/event/bot/MemberJoinRequestEvent.java
@@ -1,4 +1,19 @@
 package pub.gdt.project.daemon.event.bot;
 
-public class MemberJoinRequestEvent {
+public final class MemberJoinRequestEvent extends BotEvent {
+    private final long applicantQQ, groupId;
+    
+    public MemberJoinRequestEvent(Bot source, long internalEventId, long applicantQQ, long groupId) {
+        super(source, internalEventId);
+        this.applicantQQ = applicantQQ;
+        this.groupId = groupId;
+    }
+
+    public long getApplicantQQ() {
+        return applicantQQ;
+    }
+
+    public long getGroupId() {
+        return groupId;
+    }
 }
